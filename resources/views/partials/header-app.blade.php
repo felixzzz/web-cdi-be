e67197d214b175b7fa3b83a1346293f0b45e6cd3
Naufal <nahanifg@gmail.com>
2025-03-24 10:35:19 +0700

    update fonts

--- resources/views/partials/header-app.blade.php
+++ resources/views/partials/header-app.blade.php
@@ -1,42 +1,42 @@
 <style>
     @font-face {
         font-family: 'GT Eesti Pro Display';
-        src: url('{{ Vite::asset('resources/fonts/GTEestiProDisplay-Thin.ttf') }}') format('truetype');
+        src: url('{{ asset('assets/dist/fonts/GTEestiProDisplay-Thin.ttf') }}') format('truetype');
         font-weight: 100;
         font-style: normal;
         font-display: swap;
     }
     @font-face {
         font-family: 'GT Eesti Pro Display';
-        src: url('{{ Vite::asset('resources/fonts/GTEestiProDisplay-Light.ttf') }}') format('truetype');
+        src: url('{{ asset('assets/dist/fonts/GTEestiProDisplay-Light.ttf') }}') format('truetype');
         font-weight: 300;
         font-style: normal;
         font-display: swap;
     }
     @font-face {
         font-family: 'GT Eesti Pro Display';
-        src: url('{{ Vite::asset('resources/fonts/GTEestiProDisplay-Regular.ttf') }}') format('truetype');
+        src: url('{{ asset('assets/dist/fonts/GTEestiProDisplay-Regular.ttf') }}') format('truetype');
         font-weight: 400;
         font-style: normal;
         font-display: swap;
     }
     @font-face {
         font-family: 'GT Eesti Pro Display';
-        src: url('{{ Vite::asset('resources/fonts/GTEestiProDisplay-Medium.ttf') }}') format('truetype');
+        src: url('{{ asset('assets/dist/fonts/GTEestiProDisplay-Medium.ttf') }}') format('truetype');
         font-weight: 500;
         font-style: normal;
         font-display: swap;
     }
     @font-face {
         font-family: 'GT Eesti Pro Display';
-        src: url('{{ Vite::asset('resources/fonts/GTEestiProDisplay-Bold.ttf') }}') format('truetype');
+        src: url('{{ asset('assets/dist/fonts/GTEestiProDisplay-Bold.ttf') }}') format('truetype');
         font-weight: 700;
         font-style: normal;
         font-display: swap;
     }
     @font-face {
         font-family: 'GT Eesti Pro Display';
-        src: url('{{ Vite::asset('resources/fonts/GTEestiProDisplay-BoldItalic.ttf') }}') format('truetype');
+        src: url('{{ asset('assets/dist/fonts/GTEestiProDisplay-BoldItalic.ttf') }}') format('truetype');
         font-weight: 700;
         font-style: italic;
         font-display: swap;
