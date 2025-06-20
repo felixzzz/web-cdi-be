cce1096db4b715f316837790bff36d30fbb134fe
Naufal <nahanifg@gmail.com>
2025-06-20 13:36:51 +0700

    fixing video ios

--- resources/js/Pages/Home/components/HomeBanner.vue
+++ resources/js/Pages/Home/components/HomeBanner.vue
@@ -2,7 +2,7 @@
     <div class="relative overflow-hidden aspect-[4/3] lg:aspect-video w-full py-[5%] lg:py-[8%] flex items-end">
         <video
             class="absolute top-0 left-0 right-0 bottom-0 w-full aspect-[4/3] lg:aspect-video object-cover"
-            :src="content.home_banner?.file_url" autoplay muted loop
+            :src="content.home_banner?.file_url" autoplay muted loop playsinline
         ></video>
         <div class="overlay-banner h-full w-full absolute left-0 right-0 top-0 bottom-0"></div>
         <container>
