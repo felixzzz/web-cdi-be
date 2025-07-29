83f8ac3fe6c3c9c7eb2389267a5b184d40f32690
Naufal <nahanifg@gmail.com>
2025-07-29 15:25:06 +0700

    update: font size

--- resources/js/Pages/ContactUs/components/FormSection.vue
+++ resources/js/Pages/ContactUs/components/FormSection.vue
@@ -18,12 +18,12 @@
                             <p class="text-neutral-8 text-sm">{{ content.office.localized_main.address }}</p>
 
                             <div class="flex items-center gap-4 text-neutral-8">
-                                <div class="flex items-center text-sm gap-2" v-if="content.office.localized_main.phone">
+                                <div class="flex items-center text-xs gap-2" v-if="content.office.localized_main.phone">
                                     <img :src="asset('assets/frontend/icons/ic_phone.svg')" alt="">
                                     {{ content.office.localized_main.phone }}
                                 </div>
 
-                                <div class="flex items-center text-sm gap-2" v-if="content.office.localized_main.fax">
+                                <div class="flex items-center text-xs gap-2" v-if="content.office.localized_main.fax">
                                     <img :src="asset('assets/frontend/icons/ic_printer.svg')" alt="">
                                     {{ content.office.localized_main.fax }}
                                 </div>
