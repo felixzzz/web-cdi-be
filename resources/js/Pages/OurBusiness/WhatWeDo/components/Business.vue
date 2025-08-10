8a17986eedaa4830038bff93afd553bbcc0ae56a
Naufal <nahanifg@gmail.com>
2025-08-10 20:17:06 +0700

    update: fotn size

--- resources/js/Pages/OurBusiness/WhatWeDo/components/Business.vue
+++ resources/js/Pages/OurBusiness/WhatWeDo/components/Business.vue
@@ -16,7 +16,7 @@
                 <h1
                     :class="{
                         'font-medium text-2xl lg:text-[38px] lg:leading-[44px]': hoverIndex === index,
-                        'font-medium text-2xl lg:text-[52px] lg:leading-[60px]': hoverIndex !== index,
+                        'font-medium text-2xl lg:text-[32px] 2xl:text-[52px] 2xl:leading-[60px]': hoverIndex !== index,
                     }"
                 >{{ item.title }}</h1>
                 <div
